58342e9fba4bbfccef1e603e5f8b6e0878d84771
Vi Trần <vitran@Vis-MacBook-Air.lan>
2026-05-30 11:00:15 +0700

    Remove hardcoded old Neon URL

--- scripts/check_db_size.ts
+++ scripts/check_db_size.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: "postgresql://neondb_owner:npg_L2T3FDhUQqkP@ep-summer-pond-aop106lt.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-    }
-  }
-});
+const prisma = new PrismaClient();
 
 async function main() {
   const counts = await Promise.all([
